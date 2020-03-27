cd8a76bd9bae7c2d181d7852f6a680821d22bd4d
shanshuo0918 <shanshuo0918@gmail.com>
2020-03-27 10:42:55 +0800

    Push angular code

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'influencer';
+
+  constructor(public auth: AngularFireAuth) {
+    }
+    login() {
+    this.auth.signInWithPopup(new auth.GoogleAuthProvider());
+    }
+    logout() {
+    this.auth.signOut();
+    }
+}
